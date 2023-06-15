d09e8e34e544dcd229c89c1353005a047b42c67a
PedroAbreu11 <96292850+PedroAbreu11@users.noreply.github.com>
2023-06-16 00:18:18 +0100

    Portfolio v1.3 - settings.py allowed_host pythonanywhere - licenciatura 100% done

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -26,9 +25,9 @@ SECRET_KEY = 'django-insecure-95-1tmp!h9d0o6+^2jw0!$%n%k_*-56(s=+p^+)w15=10x_41v
 DEBUG = True
 
 ALLOWED_HOSTS = [
+    '127.0.0.1',
     'a22110364.pythonanywhere.com'
 ]
-
 
 # Application definition
 
@@ -72,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -82,7 +80,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -102,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -113,7 +109,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
